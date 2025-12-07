75d1b58d95c15479e798a6c41988e81f790550a9
Sofiane Chatelet <184280384+PepsPrime@users.noreply.github.com.>
2025-12-07 16:40:22 +0100

    Added project as class property, initialize as default if ommited at constructor call

--- src/index.js
+++ src/index.js
@@ -5,20 +5,26 @@ import './styles.css'
 class Item {
   // track nb of items
   static nbOfItem = 0
+  #project
   #title
   #description
   #dueDate
   #priority
 
-  constructor (title, description, dueDate, priority) {
+  constructor (title, description, dueDate, priority,project = "Default") {
     this.#title = title
     this.#description = description
     this.#dueDate = dueDate
     this.#priority = priority
+    this.#project = project
     Item.nbOfItem++
   }
 
   // getters and setters
+  static getNbOfItem () {
+    console.log(Item.nbOfItem)
+  }
+  
   getTitle () {
     console.log(this.#title)
   }
@@ -48,6 +54,14 @@ class Item {
   setPriority (newPriority) {
     this.#priority = newPriority
   }
+
+  getProject () {
+    console.log(this.#project)
+  }
+  
+  setProject (projectName) {
+    this.#project = projectName
+  }
   
   static createItem (title, description, dueDate, priority) {
     let newItem = new Item(title, description, dueDate, priority)
@@ -57,10 +71,13 @@ class Item {
 
 }
 
-
 let test = Item.createItem('Urgent task title', 'I really must accomplish this task', '25 December 2025', 2)
 let test2 = Item.createItem("a title","a desc","a date","what priority")
 test.getTitle()
 test.setTitle("Not so urgent title")
 test.getTitle()
 test2.getTitle()
+test.getProject()
+test.setProject("Summer 2026")
+test.getProject()
+Item.getNbOfItem()
