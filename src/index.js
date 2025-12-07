c048f8444034c338169e4157db4f3a93a0699ada
Sofiane Chatelet <184280384+PepsPrime@users.noreply.github.com.>
2025-12-07 15:32:23 +0100

    Class Item implemented with getters/setters and static method to create an instance

--- src/index.js
+++ src/index.js
@@ -1,2 +1,66 @@
-import "./styles.css"
-alert("hello")
+import './styles.css'
+
+// Todo item class
+
+class Item {
+  // track nb of items
+  static nbOfItem = 0
+  #title
+  #description
+  #dueDate
+  #priority
+
+  constructor (title, description, dueDate, priority) {
+    this.#title = title
+    this.#description = description
+    this.#dueDate = dueDate
+    this.#priority = priority
+    Item.nbOfItem++
+  }
+
+  // getters and setters
+  getTitle () {
+    console.log(this.#title)
+  }
+  setTitle (newTitle) {
+    this.#title = newTitle
+  }
+  getDescription () {
+    console.log(this.#description)
+  }
+
+  setDescription (newDescription) {
+    this.#description = newDescription
+  }
+
+  getDueDate () {
+    console.log(this.#dueDate)
+  }
+
+  setDueDate (newDueDate) {
+    this.#dueDate = newDueDate
+  }
+
+  getPriority () {
+    console.log(this.#priority)
+  }
+
+  setPriority (newPriority) {
+    this.#priority = newPriority
+  }
+  
+  static createItem (title, description, dueDate, priority) {
+    let newItem = new Item(title, description, dueDate, priority)
+    console.log(Item.nbOfItem)
+    return newItem
+  }
+
+}
+
+
+let test = Item.createItem('Urgent task title', 'I really must accomplish this task', '25 December 2025', 2)
+let test2 = Item.createItem("a title","a desc","a date","what priority")
+test.getTitle()
+test.setTitle("Not so urgent title")
+test.getTitle()
+test2.getTitle()
